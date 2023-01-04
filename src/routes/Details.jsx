076bf1b7ebd5c9f2161690a5cb253e6ce1d2d769
Ibrohim2001 <ikhayitmirzaev@gmail.com>
2023-01-04 23:07:11 +0100

    removed useParams

--- src/routes/Details.jsx
+++ src/routes/Details.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 
@@ -7,7 +7,6 @@ import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 const Details = () => {
 
   const countryName = useLocation().pathname.split('/')[2].replace(/-/g, ' ');
-  // const countryId = useParams();
   const url = 'https://restcountries.com/v3.1';
   const [data, setData] = useState([]);
   useEffect(() => {
